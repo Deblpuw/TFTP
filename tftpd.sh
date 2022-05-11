4cdbf5912c8fca6804b5b25e978aeaa344396151
Deblpuw <104028017+Deblpuw@users.noreply.github.com>
2022-05-11 11:49:35 +0200

    Update tftpd.sh

--- tftpd.sh
+++ tftpd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt-get  update
+sudo apt-get update
 sudo apt-get install tftpd-hpa
 sudo apt-get install tftp
-ip addr | grep inet |tr -d [:alpha:]|tr -s [:space:]|cut -d : -f2 | cut -c1-13 |tail -n 2 > ip
+ip a  | grep -w inet | awk '{print $2}' | grep -v 127.0.0.1 | sed 's/\/.*//g' > ip
 IP=$(cat ip)
 DOC="/etc/default/tftpd-hpa"
 cat $DOC | sed -i 's/TFTP_ADDRESS=.*/TFTP_ADDRESS="'"echo $IP:69"'"/g' $DOC
-sudo sed -i 's/TFTP_OPTIONS="--secure".*/TFTP_OPTIONS="--secure --create"/g' /etc/default/tftpd-hpa
+sudo sed -i 's/TFTP_OPTIONS="--secure".*/TFTP_OPTIONS="--secure --create"/g' $DOC
 sudo chown tftp:tftp /srv/tftp
 sudo systemctl restart tftpd-hpa
